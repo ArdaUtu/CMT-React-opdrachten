1855997babc6f766ef3ac0dbeaa2d5f8e14f75c8
ArdaUtu <9024283@student.zadkine.nl>
2025-11-25 10:55:26 +0100

    opdracht 7 klaar

--- opdracht-07-twitter/src/App.jsx
+++ opdracht-07-twitter/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css'
-
+import CreateMessage from './components/CreateMessage'
+import MessageList from './components/MessageList'
+ 
 function App() {
-
+  const [textInput, setTextInput] = useState('');
+  const [message, setMessage] = useState([]);
+ 
+let name = "Arda";
   return (
-    <>
-      <h1>Opdracht 7 - Twitter</h1>
-    </>
-  )
-}
-
+  <>
+    <CreateMessage textInput={textInput} setMessage={setMessage} setTextInput={setTextInput} message={message} />
+ <MessageList name={name} message={message}/>
+   </>
+ 
+  );
+};
+ 
 export default App
