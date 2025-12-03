0f8d02c3f62464f0d47d74a73f754c723859474c
ArdaUtu <9024283@student.zadkine.nl>
2025-12-03 12:19:19 +0100

    opdracht 11 klaar

--- opdracht-01-books/src/components/Booklist.jsx
+++ opdracht-01-books/src/components/Booklist.jsx
@@ -2,41 +2,47 @@
 import { useState } from 'react';
 import Book from './Book';
 import BookCounter from './BookCounter';
+import booksData from '../data.js';
 
 const BookList = () => {
-  const [books, setBooks] = useState([
-    {
-      title: 'Harry Potter',
-      author: 'J.K. Rowling',
-      image: './images/book-1.png',
-    },
-    {
-      title: 'Fantasia VI',
-      author: 'Geronimo Stilton',
-      image: './images/book-2.png',
-    },
-    {
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      image: './images/book-3.png',
-    },
-  ]);
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const filteredBooks = booksData.filter((book) =>
+    book.title.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
     <section className="BookBody">
-      <BookCounter aantal={books.length} />
 
-    
-      {books.map((book, index) => (
+      <div className="search">
+        <input
+          type="text"
+          placeholder="zoek een titel op"
+          onChange={handleChange}
+          value={searchInput}
+          name="search"
+        />
+      </div>
+
+      {/* Counter voor aantal gefilterde boeken */}
+      <BookCounter aantal={filteredBooks.length} />
+
+      {/* Boekweergave */}
+      {filteredBooks.map((book, index) => (
         <Book
           key={index}
           title={book.title}
           author={book.author}
           img={book.image}
-          button={book.aantalKeerGelezen} 
-          btn={book.Btn} 
+          button={book.aantalKeerGelezen}
+          btn={book.Btn}
         />
       ))}
+
     </section>
   );
 };
---
+++ opdracht-01-books/src/data.js
@@ -0,0 +1,21 @@
+const books = [
+    
+    {
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      image: './images/images/book-1.png',
+    },
+    {
+      title: 'Fantasia VI',
+      author: 'Geronimo Stilton',
+      image: './images/images/book-2.png',
+    },
+    {
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      image: './images/images/book-3.png',
+    },
+  
+]
+
+export default books
