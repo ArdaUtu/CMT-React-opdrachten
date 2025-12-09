fb13b3d03b687857420c3ecb75fa26f03157b059
ArdaUtu <9024283@student.zadkine.nl>
2025-12-09 11:58:36 +0100

    upd

--- opdracht-12-weather-app/src/components/WeatherInfo.jsx
+++ opdracht-12-weather-app/src/components/WeatherInfo.jsx
@@ -6,6 +6,9 @@ const WeatherInfo = ({ weather }) => {
           <h1>{weather.name}</h1>
           <p>Temperatuur: {weather.main.temp} °C</p>
           <p>Weer: {weather.weather[0].description}</p>
+          <p>Luchtvochtigheid: {weather.main.humidity} %
+           <p>wind snelheid: {weather.wind.speed} m/s</p>
+          </p>
         </section>
       ) : (
         <p>Het is weer aan het laden</p>
